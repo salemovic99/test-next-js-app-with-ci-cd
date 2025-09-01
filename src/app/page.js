33f808c727f3fb3270487db9073c69dbf70d2e77
salemovic <jakihop889@gamil.com>
2025-09-01 15:18:55 +0300

    edit home page

--- src/app/page.js
+++ src/app/page.js
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
       <h2 className="text-4xl sm:text-6xl font-bold text-center">
-        Welcome to <span className="text-blue-600">Next.js Edit</span>!
+        Welcome to <span className="text-blue-600">Next.js Edit after github workflows</span>!
       </h2>
     </div>
   );
